3a24caf82eecb322251526678cde7ff66d6fd0cd
gautam, prasanna <prasannagautam@gmail.com>
2013-02-08 15:35:22 -0500

    removing jsmin dependency for now

--- app.rb
+++ app.rb
@@ -2,22 +2,22 @@
 require 'sinatra'
 require 'haml'
 
-require_relative 'minify_resources'
+#require_relative 'minify_resources'
 class MyApp < Sinatra::Application
 	enable :sessions
 
-	configure :production do
-		set :haml, { :ugly=>true }
-		set :clean_trace, true
-		set :css_files, :blob
-		set :js_files,  :blob
-		MinifyResources.minify_all
-	end
+	#configure :production do
+	#	set :haml, { :ugly=>true }
+	#	set :clean_trace, true
+	#	set :css_files, :blob
+	#	set :js_files,  :blob
+	#	MinifyResources.minify_all
+	#end
 
-	configure :development do
-		set :css_files, MinifyResources::CSS_FILES
-		set :js_files,  MinifyResources::JS_FILES
-	end
+	#configure :development do
+	#	set :css_files, MinifyResources::CSS_FILES
+	#	set :js_files,  MinifyResources::JS_FILES
+	#end
 
 	helpers do
 		include Rack::Utils
